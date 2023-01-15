be43fadd9d9e40e63c869b654a6366a84e2f6c0a
thiagoDRangel <info.tec.campos@gmail.com>
2023-01-14 23:04:47 -0300

    substituindo o bind por arrow function

--- Exercícios/08-particularidades-react/src/App.jsx
+++ Exercícios/08-particularidades-react/src/App.jsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-class App extends React.Component {
-  constructor() {
-    super()
-    // A função abaixo vincula "manualmente" o `this` à nossa função
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick() {
-    /* Agora esse log retorna o objeto `this`, já acessível para nossa função!
-    Com isso, podemos acessar as `props`, estado do componente (ainda vamos ver como!)
-    e tudo o mais daqui de dentro */
-    console.log(this)
+class App extends Component {
+  handleClick = () => {
     console.log('Clicou!')
+    console.log(this)
   }
 
   render() {
-    return <button onClick={this.handleClick}>Meu botão</button>
+    return (
+      <button
+        onClick={ this.handleClick }
+      >
+        Meu botão
+      </button>
+    );
   }
 }
 
