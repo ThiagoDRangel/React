e7872c62d3b8706403c08988e060ad5de0417174
thiagoDRangel <info.tec.campos@gmail.com>
2023-01-15 00:12:05 -0300

    Usando bind dentro do construtor

--- Exercícios/08-particularidades-react/src/App.jsx
+++ Exercícios/08-particularidades-react/src/App.jsx
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  handleClick = () => {
-    console.log('Clicou!')
-    console.log(this)
+  constructor() {
+    super();
+    this.handleButtonOne = this.handleButtonOne.bind(this);
+    this.handleButtonTwo = this.handleButtonTwo.bind(this);
+    this.handleButtonThree = this.handleButtonThree.bind(this);
+  }
+  handleButtonOne() {
+    console.log('Clicou no botão 1!');
+  }
+
+  handleButtonTwo() {
+    console.log('Clicou no botão 2!');
+  }
+
+  handleButtonThree() {
+    console.log('Clicou no botão 3!');
   }
 
   render() {
     return (
-      <button
-        onClick={ this.handleClick }
-      >
-        Meu botão
-      </button>
+      <section>
+        <button
+          type="button"
+          onClick={ this.handleButtonOne }
+        >
+          Botão 01
+        </button>
+        <button
+          type="button"
+          onClick={ this.handleButtonTwo }
+        >
+          Botão 02
+        </button>
+        <button
+          type="button"
+          onClick={ this.handleButtonThree }
+        >
+          Botão 03
+        </button>
+      </section>
     );
   }
 }
