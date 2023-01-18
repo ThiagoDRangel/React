0112dcba2c8cac3015fbdb8e48186b3981fe86f7
thiagoDRangel <info.tec.campos@gmail.com>
2023-01-18 15:48:23 -0300

    add functionality on button

--- tests/Exercícios/02-testing-events/src/components/ValidEmail.jsx
+++ tests/Exercícios/02-testing-events/src/components/ValidEmail.jsx
@@ -13,6 +13,13 @@ const ValidEmail = (props) => {
     <div>
       <h2 data-testid="id-email-user">{`Valor: ${email}`}</h2>
       <h3>{(verifyEmail(email) ? 'Email Válido' : 'Email Inválido')}</h3>
+      {
+           email && (
+            <h3 data-testid="id-is-email-valid" className={verifyEmail(email) ? 'green-text' : 'red-text'}>
+               {(verifyEmail(email) ? 'Email Válido' : 'Email Inválido')}
+             </h3>
+           )
+         }
     </div>
   );
 };
