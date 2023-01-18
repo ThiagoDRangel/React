c152c8725f35a4f611aa7ed80c15cbe7a2c3c4d3
thiagoDRangel <info.tec.campos@gmail.com>
2023-01-18 13:55:46 -0300

    Validating email

---
+++ tests/Exercícios/02-testing-events/src/tests/ValidEmail.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ValidEmail from '..components/ValidEmail';
+import { render, screen } from '@testing-library/react';
+
+// Teste se o email é válido
+test('Testando um componente, caso o email seja válido.', () => {
+  const EMAIL_USER = 'email@email.com';
+  render(<ValidEmail email={ EMAIL_USER } />);
+  const isValid = screen.getByText('Email Válido');
+  expect(isValid).toBeInTheDocument();
+});
+
+// teste de email inválido
+test('Testando um componente, caso o email seja inválido.', () => {
+    const EMAIL_USER = 'emailerrado';
+    render(<ValidEmail email={ EMAIL_USER } />);
+    const isValid = screen.getByText('Email Inválido');
+    expect(isValid).toBeInTheDocument();
+  });
+  
