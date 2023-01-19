d2a150880e447b77d97e5528d39fe0899d674e4f
thiagoDRangel <info.tec.campos@gmail.com>
2023-01-19 12:04:12 -0300

    add functions

--- Projetos/pokemon-cards/src/App.jsx
+++ Projetos/pokemon-cards/src/App.jsx
@@ -9,33 +9,49 @@ class App extends Component {
   };
 }
 
-  render() {
-    const { cards, pokemon } = this.state;
-    return (
-      <main className="App">
-        <img src={ logo } alt="Logo" />
+handleChange(event) {
+  this.setState({
+    pokemon: event.target.value,
+  });
+}
 
-        <h3>Pesquise um Pokemon</h3>
-        <div>
-          <input
-            type="text"
-            className="form_input"
-            onChange=""
-            data-testid="input-text"
-            value=""
-          />
-          <button
-            onClick=""
-          >
-            Pesquisar
-          </button>
-        </div>
-        <div>
-          
-        </div>
-      </main>
-    );
-  }
+handleClick(pokemon) {
+  fetch(`https://api.pokemontcg.io/v1/cards?name=${pokemon}`)
+    .then((response) => response.json())
+    .then((pokemonCards) => this.setState({
+      cards: pokemonCards.cards[0],
+      pokemon: ''
+    }));
+}
+
+
+render() {
+  const { cards, pokemon } = this.state;
+  return (
+    <main className="App">
+      <img src={ logo } alt="Logo" />
+
+      <h3>Pesquise um Pokemon</h3>
+      <div>
+        <input
+          type="text"
+          className="form_input"
+          onChange=""
+          data-testid="input-text"
+          value=""
+        />
+        <button
+          onClick=""
+        >
+          Pesquisar
+        </button>
+      </div>
+      <div>
+
+      </div>
+    </main>
+  );
+}
 }
 
 export default App;
